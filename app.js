916ad99aa1dcbd0c48013deaca17af8232e2faee
kamel Barakat <kamelbarakat96@yahoo.com>
2022-10-22 09:23:35 -0400

    Complete

--- app.js
+++ app.js
@@ -1,112 +1,63 @@
-// map object
-const myMap = {
-  coordinates: [],
-  businesses: [],
-  map: {},
-  markers: {},
-  
-  // build leaflet map
-  buildMap() {
-    this.map = L.map('map', {
-    center: this.coordinates,
-    zoom: 11,
-    });
-    // add openstreetmap tiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    minZoom: '15',
-    }).addTo(this.map)
-    // create and add geolocation marker
-    const marker = L.marker(this.coordinates)
-    marker
-    .addTo(this.map)
-    .bindPopup('<p1><b>Current Location</b><br></p1>')
-    .openPopup()
-  },
-  
-  // add business markers
-  addMarkers() {
-    for (var i = 0; i < this.businesses.length; i++) {
-    this.markers = L.marker([
-      this.businesses[i].lat,
-      this.businesses[i].long,
-    ])
-      .bindPopup(`<p1>${this.businesses[i].name}</p1>`)
-      .addTo(this.map)
-    }
-  },
- }
-  
- // get coordinates via geolocation api
- async function getCoords(){
-  const pos = await new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject)
+// Get the user's coordinates:
+async function getCoords(){
+  pos = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject)
   });
   return [pos.coords.latitude, pos.coords.longitude]
- }
-  
- // get foursquare businesses
- async function getFoursquare(business) {
-  const options = {
-    method: 'GET',
-    headers: {
-    Accept: 'application/json',
-    Authorization: 'fsq3c439Uf+e7R8k3HHmz6ja6t4W7OX7FcFwWKsVlq6Ea/A='
-    }
-  }
-  let limit = 5
-  let lat = myMap.coordinates[0]
-  let lon = myMap.coordinates[1]
-  let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
-  let data = await response.text()
-  let parsedData = JSON.parse(data)
-  
-  let businesses = parsedData.results
-  
-  return businesses
-  
- }
- // process foursquare array
- function processBusinesses(data) {
-  let businesses = data.map((element) => {
-    
-    let location = {
-      name: element.name,
-      lat: element.geocodes.main.latitude,
-      long: element.geocodes.main.longitude
-    };
-    
-    return location
-  })
-  return businesses
- }
-  
-  
- // event handlers
- // window load
- window.onload = async () => {
-  const coords = await getCoords()
-  myMap.coordinates = coords
-  myMap.buildMap()
- }
-  
- // business submit button
- document.getElementById('submit').addEventListener('click', async (event) => {
+}
+
+//created a load up window
+// On load, build ads:
+window.onload = async () => {
+  myMap = L.map('map',{
+      center: await getCoords(),
+      zoom:12,
+  });
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: '15',
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a>'
+      }).addTo(myMap);
+
+      //geolocation marker
+      const marker = L.marker([pos.coords.latitude, pos.coords.longitude]);
+      marker.addTo(myMap).bindPopup('<p>You Are Here</p>').openPopup();
+};
+
+
+//Create an event to ask for the user location
+document.querySelector('#submit').addEventListener('click', async (event) =>{
   event.preventDefault()
-  let business = document.getElementById('business').value
-  let data = await getFoursquare(business)
-  myMap.businesses = processBusinesses(data)
-  myMap.addMarkers()
- })
+  let business = document.querySelector('#business').value;
+  let latlong = await getCoords()
+  let lat = latlong[0]
+  let long = latlong[1]
+  let data = await getFourSquare(business, lat, long);
+  let response = data.json();
+  console.log(response)
+  //myMap.businesses = processBusinesses(response);
+  //myMap.addMarkers();
 
- // create red pin marker
-var redPin = L.icon({
-    iconUrl: './redpin.png',
-
-    iconSize: [38, 38], // size of the icon
-    shadowSize: [50, 64], // size of the shadow
-    iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
+
+async function getFourSquare(business, lat, long){
+const options = {
+  method: 'GET',
+  headers: {
+      accept: 'application/json',
+      Authorization: 'fsq3c439Uf+e7R8k3HHmz6ja6t4W7OX7FcFwWKsVlq6Ea/A='
+  }
+  };
+
+  // let params = new URLSearchParams({
+  //     query: business,
+  //     ll: ${lat},${long},
+  //     limit: 5 
+  // });
+  
+
+
+
+  return await fetch('https://api.foursquare.com/v3/places/search?&${params}', options)
+
+}
